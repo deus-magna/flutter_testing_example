22fda91cf4db8aa46bb2be4e411bb784e2ff2cff
Renato Montufar <rmontufar1792@gmail.com>
2020-12-06 09:16:05 -0600

    first commit

---
+++ test/fetch_post_test.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:http/http.dart' as http;
+import 'package:unit_testing/post.dart';
+
+// Create a MockClient using the Mock class provided by the Mockito package.
+// Create new instances of this class in each test.
+class MockClient extends Mock implements http.Client {}
+
+main() {
+  group('fetchPost', () {
+    test('returns a Post if the http call completes successfully', () async {
+      final client = MockClient();
+
+      // Use Mockito to return a successful response when it calls the
+      // provided http.Client.
+      when(client.get('https://jsonplaceholder.typicode.com/posts/1'))
+          .thenAnswer((_) async => http.Response('{"title": "Test"}', 200));
+
+      expect(await fetchPost(client), isA<Post>());
+    });
+
+    test('throws an exception if the http call completes with an error', () {
+      final client = MockClient();
+
+      // Use Mockito to return an unsuccessful response when it calls the
+      // provided http.Client.
+      when(client.get('https://jsonplaceholder.typicode.com/posts/1'))
+          .thenAnswer((_) async => http.Response('Not Found', 404));
+
+      expect(fetchPost(client), throwsException);
+    });
+  });
+}
